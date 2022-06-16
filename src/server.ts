12a33f215edbae000e9b623e343d3eee04b98ba9
Vijay <vijay@iMac-di-IMac.fritz.box>
2022-06-16 12:21:40 +0200

    Adds product routes

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,73 @@
 import express, {Request, Response} from 'express';
 import mongoose from 'mongoose';
+import { ProductModel } from './models/product';
 
 
 
 const app = express();
+const port = process.env.PORT || 3006;
 
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World');
-});     
+});  
+
+app.get('/products', async (req: Request, res: Response) => {
+
+    try {
+        const products = await ProductModel.find();
+        res.send(products);
+    } catch (error) {
+        res.status(500).send(error);
+    }    
+});
+
+app.post('/product', async (req: Request, res: Response) => {
+      try {
+        const product = new ProductModel(req.body);
+        await product.save();
+        res.send(product);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.get('/product/:id', async (req: Request, res: Response) => {
+    try {
+        const product = await ProductModel.findById(req.params.id);
+        res.send(product);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.put('/product/:id', async (req: Request, res: Response) => {
+    try {
+        const product = await ProductModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.send(product);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.post('/product/:id/picture', async (req: Request, res: Response) => {
+    try {
+        const product = await ProductModel.findById(req.body.id);
+        product.image = req.body.image;
+        await product.save();
+        res.send(product);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
 
 
 const start = async () : Promise<void> => {
     try{
-        await mongoose.connect('mongodb+srv://lineapp:lineapp@cluster0.ra7s2.mongodb.net/QuantumEchoDB?retryWrites=true&w=majority');
+        await mongoose.connect(process.env.MONGOdb_URI || 'mongodb://localhost/ts-node-starter');
         console.log('Connected to MongoDB');
-        app.listen(3006, () => console.log(`Server started on port 3006`));
+        app.listen(port, () => console.log(`Server started on port ${port}`));
     }catch(err){
         console.log("Oops! something wen wrong!",err);
     }
